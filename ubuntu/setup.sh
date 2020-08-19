0e4caa49d8784334a06f41402014b9ebd8e3d6dc
chaitanyapotti <chaitanya.potti@gmail.com>
2020-08-20 00:32:47 +0800

    add ubuntu setup

---
+++ ubuntu/setup.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+sudo apt update -y
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo apt update -y
+sudo apt install docker-ce
+sudo usermod -aG docker ${USER}
+sudo curl -L https://github.com/docker/compose/releases/download/1.26.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
